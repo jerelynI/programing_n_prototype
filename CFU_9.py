d6b3e7e02824e2987fd51c49c9af7134e298e3fc
jerelynI <144715038+jerelynI@users.noreply.github.com>
2024-10-17 09:25:04 -0400

    Create CFU_9.py

---
+++ CFU_9.py
@@ -0,0 +1,16 @@
+jerelyn
+#10-17-24
+#CFU 9 Guessing Game 
+import random
+dice_rolls= random. randint (1,6)
+score= 0
+rolls = int(input(*How many rolls do you want to play? *))
+guess = int(input("Guess the number 1-6: "))
+if guess
+== "dice_rolls":
+print("Correct! You gain 6 points.")
+score t=6
+else:
+print("Wrong! You lose 1 point.")
+score =1
+print ("your final score is" + str(score))
