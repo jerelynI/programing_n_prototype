d89a2d94e5614ef78bb633f126099500d8055a15
jerelynI <144715038+jerelynI@users.noreply.github.com>
2024-10-30 09:16:28 -0400

    Update CFU12.py

--- CFU12.py
+++ CFU12.py
@@ -1,9 +1,29 @@
 #jerelyn
 #10-29-2024
 #CFU12
-password = "simonsnyc"
-userInput = input("enter password: ")
-while password != userInput :
-    print("wrong password!")
+def version1():
+    password = "simonsnyc"
     userInput = input("enter password: ")
+    
+    while password != userInput :
+        print("wrong password!")
+        userInput = input("enter password: ")
 print("correct ! you may enter... ")
+version1()
+
+
+def version2():
+    password = "simonsnyc"
+    userInput = input("enter password: ")
+    num_guesses = 0
+    
+    while password != userInput and num_guesses < 3:
+        print("wrong password!")
+        userInput = input("enter password: ")
+        num_guesses +=1
+        
+    if password == "simonsnyc":
+        print("correct! you may enter...")
+    else:
+        print("wrong password!")
+version2()
