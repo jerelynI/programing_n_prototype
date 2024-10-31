955897c1470f99ab79eb52530b350febccbd8afb
jerelynI <144715038+jerelynI@users.noreply.github.com>
2024-10-31 09:26:18 -0400

    Update CFU12.py

--- CFU12.py
+++ CFU12.py
@@ -1,29 +1,36 @@
 #jerelyn
 #10-29-2024
 #CFU12
-def version1():
+def v1():
     password = "simonsnyc"
     userInput = input("enter password: ")
     
     while password != userInput :
         print("wrong password!")
         userInput = input("enter password: ")
-print("correct ! you may enter... ")
-version1()
-
-
-def version2():
-    password = "simonsnyc"
-    userInput = input("enter password: ")
-    num_guesses = 0
-    
-    while password != userInput and num_guesses < 3:
-        print("wrong password!")
-        userInput = input("enter password: ")
-        num_guesses +=1
         
-    if password == "simonsnyc":
-        print("correct! you may enter...")
-    else:
+    print("correct ! you may enter... ")
+
+#v1()
+def v2():
+    password = "simonsnyc"
+    userInput = input("Enter passcode:")
+    i = 0
+    while password != userInput and i < 3:
         print("wrong password!")
-version2()
+        i+=1
+        userInput = input("Enter passcode: ")
+        if password == userInput:
+            print("correct! You may Enter...")
+            
+#v2()
+def main():
+    version = input("choose 1 or 2")
+    if version == "1":
+        v1()
+    elif version == "2":
+        v2()
+    else:
+        print("that isnt a choice")
+
+main()
